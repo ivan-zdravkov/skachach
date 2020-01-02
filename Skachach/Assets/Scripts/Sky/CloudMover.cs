de89ec987d8824b8c4990ed96a0cbbabf8e862eb
i-z <IvanZdravkovBG@gmail.com>
2020-01-02 15:54:42 +0200

    Basic sky!

---
+++ Skachach/Assets/Scripts/Sky/CloudMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMover : MonoBehaviour
+{
+    [Range(1, 20)][SerializeField] float minSpeed = 5f;
+    [Range(1, 20)][SerializeField] float maxSpeed = 15f;
+
+    float speed;
+
+    void Start()
+    {
+        if (this.minSpeed > this.maxSpeed)
+            throw new System.ArgumentException("minSpeed cannot be larger than maxSpeed.");
+
+        this.speed = Random.Range(this.minSpeed, this.maxSpeed);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime * this.speed / 10;
+
+        if (transform.position.x < -21f)
+            Destroy(this.gameObject);
+    }
+}
--- Skachach/Assets/Scripts/Sky/CloudSpawner.cs
+++ Skachach/Assets/Scripts/Sky/CloudSpawner.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CloudSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CloudSettings[] cloudSettings;
+
+    [SerializeField] GameObject[] farClouds;
+    [SerializeField] GameObject[] nearClouds;
+
+    private void Start()
     {
-        
+        this.cloudSettings = new CloudSettings[]
+        {
+            new CloudSettings(z: 9.75f, minX: 2f, maxX: 4f, clouds: this.farClouds),
+            new CloudSettings(z: 9.25f, minX: 0f, maxX: 3f, clouds: this.farClouds),
+            new CloudSettings(z: 8.75f, minX: -3f, maxX: -1f, clouds: this.nearClouds),
+            new CloudSettings(z: 8.25f, minX: -8f, maxX: -4f, clouds: this.nearClouds)
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    private class CloudSettings
     {
-        
+        public float Z { get; set; }
+
+        public float MinX { get; set; }
+
+        public float MaxX { get; set; }
+
+        public GameObject[] Clouds { get; set; }
+
+        public CloudSettings(float z, float minX, float maxX, GameObject[] clouds)
+        {
+            if (minX > maxX)
+                throw new ArgumentException("minX cannot be largen than maxX");
+
+            this.Z = z;
+            this.MinX = minX;
+            this.MaxX = maxX;
+            this.Clouds = clouds;
+        }
     }
 }
