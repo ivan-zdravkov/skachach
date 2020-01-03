a4c2c50daa07beff5952fd030cf8dc7b9399b9a7
i-z <IvanZdravkovBG@gmail.com>
2020-01-03 17:21:49 +0200

    Mountains color fixed.

--- Skachach/Assets/Scripts/Sky/CloudSpawner.cs
+++ Skachach/Assets/Scripts/Sky/CloudSpawner.cs
@@ -24,7 +24,7 @@ public class CloudSpawner : MonoBehaviour
             new CloudSettings(z: 9.75f, minY: 3f, maxY: 5f, clouds: this.farClouds),
             new CloudSettings(z: 9.25f, minY: 1f, maxY: 4f, clouds: this.farClouds),
             new CloudSettings(z: 8.75f, minY: -2f, maxY: 0f, clouds: this.nearClouds),
-            new CloudSettings(z: 8.25f, minY: -4f, maxY: -2f, clouds: this.nearClouds),
+            new CloudSettings(z: 8.25f, minY: -3f, maxY: -1f, clouds: this.nearClouds),
             new CloudSettings(z: 8.25f, minY: -2f, maxY: 0f, clouds: this.nearClouds)
         };
 
