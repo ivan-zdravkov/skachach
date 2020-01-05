2816cad33addc4b12559040c70237ce0e6b23340
i-z <IvanZdravkovBG@gmail.com>
2020-01-06 00:43:33 +0200

    Run and Jump.

--- Skachach/Assets/Character/Scripts/Character.cs
+++ Skachach/Assets/Character/Scripts/Character.cs
@@ -1,24 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Character : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 5.0f;
+    [SerializeField] float jumpHeight = 150f;
+
+    private Vector2 moveVector;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Run();
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log("Fire!");
+        this.moveVector = context.ReadValue<Vector2>();
+
+        this.Jump();
+    }
+
+    private void Run()
+    {
+        if (this.moveVector != null && this.moveVector.x != 0)
+        {
+            transform.position = transform.position + new Vector3(
+                x: this.moveVector.x * this.movementSpeed * Time.deltaTime,
+                y: 0f,
+                z: 0f
+            );
+        }
+    }
+
+    private void Jump()
+    {
+        if(this.moveVector != null && this.moveVector.y > 0)
+        {
+            transform.position = transform.position + new Vector3(
+                x: 0f,
+                y: this.moveVector.y * this.jumpHeight * Time.deltaTime,
+                z: 0f
+            );
+        }
     }
 }
