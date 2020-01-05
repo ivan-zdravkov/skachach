5dcd2d396833e325d896a7ea3bcea69bc5dce004
i-z <IvanZdravkovBG@gmail.com>
2020-01-06 00:53:34 +0200

    Move animation and flip done.

--- Skachach/Assets/Character/Scripts/Character.cs
+++ Skachach/Assets/Character/Scripts/Character.cs
@@ -6,14 +6,21 @@ using UnityEngine.InputSystem;
 
 public class Character : MonoBehaviour
 {
+    private const string AnimatorRunning = "IsRunning";
+    private const string AnimatorJumping = "IsJumping";
+    private const string AnimatorSliding = "IsSliding";
+    private const string AnimatorIsDead = "IsDead";
+
     [SerializeField] float movementSpeed = 5.0f;
     [SerializeField] float jumpHeight = 150f;
 
     private Vector2 moveVector;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,11 +40,22 @@ public class Character : MonoBehaviour
     {
         if (this.moveVector != null && this.moveVector.x != 0)
         {
+            this.animator.SetBool(AnimatorRunning, true);
+
             transform.position = transform.position + new Vector3(
                 x: this.moveVector.x * this.movementSpeed * Time.deltaTime,
                 y: 0f,
                 z: 0f
             );
+
+            if (this.moveVector.x > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else
+                transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            this.animator.SetBool(AnimatorRunning, false);
         }
     }
 
