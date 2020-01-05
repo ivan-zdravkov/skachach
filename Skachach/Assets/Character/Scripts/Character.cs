1a85d8db0c4824f4e293fb7344d61460f85d8ace
i-z <IvanZdravkovBG@gmail.com>
2020-01-05 15:46:26 +0200

    Player Input Setup Correctly. Unity Version Update pending. Existing Bug in 2.0f

---
+++ Skachach/Assets/Character/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire!");
+    }
+}
