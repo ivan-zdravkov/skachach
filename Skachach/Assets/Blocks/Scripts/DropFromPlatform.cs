4874ba95353701f6bb8a7fe7f0d885de73df6c40
i-z <IvanZdravkovBG@gmail.com>
2020-01-08 20:40:44 +0200

    Be able to drop through platforms.

---
+++ Skachach/Assets/Blocks/Scripts/DropFromPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DropFromPlatform : MonoBehaviour
+{
+    private const float SECONDS_TO_DISABLE_COLLIDER_FOR = 0.75f;
+
+    BoxCollider2D boxCollider;
+
+    bool boxColliderDisabled = false;
+
+    private void Start()
+    {
+        this.boxCollider = this.GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (ShouldDrop())
+            Drop();
+    }
+
+    private bool ShouldDrop()
+    {
+        return this.boxCollider.enabled && (
+            Keyboard.current.sKey.wasPressedThisFrame ||
+            Keyboard.current.downArrowKey.wasPressedThisFrame);
+    }
+
+    private void Drop()
+    {
+        this.boxCollider.enabled = false;
+
+        this.StartCoroutine(ExecuteAfterTime(SECONDS_TO_DISABLE_COLLIDER_FOR));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        this.boxCollider.enabled = true;
+    }
+}
