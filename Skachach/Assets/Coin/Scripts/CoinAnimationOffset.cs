86047519b0a8e77d16e13e43156b2c06774c67d8
i-z <IvanZdravkovBG@gmail.com>
2020-01-10 01:26:35 +0200

    Coin animated.

---
+++ Skachach/Assets/Coin/Scripts/CoinAnimationOffset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimationOffset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Animator>().SetFloat("cycleOffset", (Mathf.Floor(this.transform.position.x) % 10) / 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
