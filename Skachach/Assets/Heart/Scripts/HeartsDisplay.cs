67fb24f911df16e2e7ccb04bae59895931abe710
i-z <IvanZdravkovBG@gmail.com>
2020-01-13 23:28:58 +0200

    Health Display working.

---
+++ Skachach/Assets/Heart/Scripts/HeartsDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartsDisplay : MonoBehaviour
+{
+    private int health = 3;
+
+    SpriteRenderer[] spriteRenderers;
+
+    [SerializeField] Sprite filledHeart;
+    [SerializeField] Sprite emptyHeart;
+
+    private void Start()
+    {
+        this.spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+
+        this.UpdateHealthDisplay();
+    }
+
+    public void TakeHealth()
+    {
+        if (this.health > 0)
+            this.health--;
+
+        this.UpdateHealthDisplay();
+    }
+
+    public void GainHealth()
+    {
+        if (this.health < 3)
+            this.health++;
+
+        this.UpdateHealthDisplay();
+    }
+
+    private void UpdateHealthDisplay()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Sprite sprite = (this.health <= i) ? this.emptyHeart : this.filledHeart;
+
+            this.spriteRenderers[i].sprite = sprite;
+        }
+    }
+}
