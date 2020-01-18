f8cb81e3dacabd484a6d422bfb1be26f3f19bd3c
i-z <IvanZdravkovBG@gmail.com>
2020-01-19 00:56:44 +0200

    Enemy animations.

---
+++ Skachach/Assets/Enemies/Scripts/EnemyWalk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalk : MonoBehaviour
+{
+    [Range(1f, 20f)][SerializeField] float walkSpeed = 5f;
+    [SerializeField] bool shouldTurn = true;
+
+    private Vector2 moveVector;
+    private Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        this.moveVector = Vector2.left;
+        this.rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        EnemyDirectionTurner turner = collision.gameObject.GetComponent<EnemyDirectionTurner>();
+
+        if (this.shouldTurn && turner && collision is CircleCollider2D)
+            this.Turn();
+     }
+
+    private void Turn()
+    {
+        this.moveVector.x *= -1;
+        this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+    }
+
+    private void Move()
+    {
+        this.rigidBody.velocity = new Vector2(this.moveVector.x * this.walkSpeed, this.rigidBody.velocity.y);
+    }
+}
